2eecd755d6392d3063a1ce87623ac660633a3588
JamiuKolawole04 <Jakko4real@gmail.com>
2022-02-23 07:25:30 +0100

    initial-commit

---
+++ src/components/Home.js
@@ -0,0 +1,18 @@
+import Feed from "./Feed";
+
+function Home({ posts, fetchError, isLoading }) {
+    return (
+        <main className="Home">
+            {isLoading && <p className="statusMsg">Loading posts...</p>}
+            {!isLoading && fetchError && <p className="statusMsg" style={{ color: "red" }}>{fetchError}</p>}
+            {!isLoading && !fetchError && (posts.length ? <Feed posts={posts} /> : <p className="statusMsg" style={{ marginTop: "2rem" }}>No posts to display.</p>)}
+            {/* {posts.length ? (
+                <Feed posts={posts} />
+            ) : (
+                <p style={{ marginTop: "2rem" }}>No posts to display.</p>
+            )} */}
+        </main>
+    );
+}
+
+export default Home;
---
+++ src/hooks/Usewindowsize.js
@@ -0,0 +1,30 @@
+import { useState, useEffect } from 'react';
+
+const useWindowSize = () => {
+    const [widowSize, setWindowSize] = useState({
+        width: undefined,
+        height: undefined
+    });
+
+    // run at load time
+    useEffect(() => {
+        const handleResize = () => {
+            setWindowSize({
+                width: window.innerWidth,
+                height: window.innerHeight
+            })
+        }
+
+        handleResize();
+
+        window.addEventListener("resize", handleResize);
+
+        return () => window.removeEventListener("resize", handleResize);
+
+    }, []);
+
+    return widowSize;
+
+}
+
+export default useWindowSize;
